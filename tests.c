b6bec73eb538f352b90bc7b85f901528739ab314
AlmondNMT <alexander.schwierz@student.nmt.edu>
2023-02-07 10:26:12 -0700

    memory errors still a thing

--- tests.c
+++ tests.c
@@ -1,23 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h> 
 #include <string.h>
+//#include "utils.h"
 #include "utils.c"
-#include "token.h"
+//#include "token.h"
 #include "token.c"
+#include "back.c"
 
 
 
+void test_ext_str()
+{
+    char *s = malloc(4);
+    s[0] ='\"';
+    s[1] ='5';
+    s[2] ='\\';
+    s[3] ='n';
+    s[4] ='\\';
+    s[5] ='0';
+    s[6] ='7';
+    s[7] ='7';
+    s[8] = '\\';
+    s[9] = 'r';
+    s[10] = '\"';
+
+    printf("before extract string: %s\n", s); 
+    char * t = extract_string(s);
+    //free(s);
+    printf("after extract string: %s\n", t); 
+    free(t);
+}
+void test_ext_int()
+{
+
+    char *s = malloc(4);
+    s[0] ='2';
+    s[1] ='_';
+    s[2] ='1';
+    s[3] ='_';
+
+    int i = extract_int(s);
+    free(s);
+    printf("%d", i);
+
+
+}
+
+void test_deesc()
+{
+    char *s = malloc(sizeof(char)*5);
+    s[0] = '5';
+    s[1] = '5';
+    s[2] = '\\';
+    s[3] = 'n';
+    s[4] = '5';
+    printf("before deescape: %s\n",s);
+    char *temp = deescape(s);
+    free(s);
+    printf("after deescape: %s", temp);
+    free(temp);
+    //free(temp);
+    //printf("2 %s\n", s);
+    //free(s);
+
+}
 
 int main()
-{
-		tokenlist_t *list_head = NULL;
-		list_head = malloc(sizeof(tokenlist_t));
-		list_head->next = NULL;
-
-
-        insert_node(&list_head, 8.5, "ghghg", 456, "yytext", 555, 343, "file");
-        insert_node(&list_head, 7.5, "fef", 777, "htht", 990, 5655, "fole");
-        insert_node(&list_head, 6.5, "yyy", 898, "ext", 7000, 6, "fowl");
-
-        print_list(list_head);
+{   
+    test_ext_str();
+    //test_deesc();
+    //test_ext_int();
 }
--- utils.c
+++ utils.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include <ctype.h>
 #include "utils.h"
 
 //WARNING: UNSAFE helper, need to pre-check your strlen(), or this crashes
@@ -17,7 +19,147 @@ char *substring(char *s, int start, int end)
 
 }
 
+int get_ascii(char c)
+{
+
+	switch(c){
+		case 'n': 
+			return '\n';
+			break;
+		
+		case 'a':
+			return '\a';
+			break;
+		
+		case 'f': 
+			return '\f';
+			break;
+		
+		case 'r': 
+			return '\r';
+			break;
+			
+		case 't': 
+			return '\t';
+			break;			
+			
+		case '\\': 
+			return '\x5c';
+			break;
+			
+		case '\'': 
+			return '\x27';
+			break;
+			
+		case '"': 
+			return '"';
+			break;
+		default:
+			return -1;
+
+		}
+}
+
+void panic(char *errmsg)
+{
+	printf("%s",errmsg);
+	exit(-1);
+}
+
+char *deescape(char *s)
+{   
+    char *temp = malloc(sizeof(char) * strlen(s));
+    int temp_index = 0;
+    int s_index = 0;
+    while(s[s_index] != '\0')
+    {
+        if(s[s_index] == '\\')
+        {
+            s_index++;
+            if(!isdigit(s[s_index]))
+            {	
+            	if((temp[temp_index] = get_ascii(s[s_index])) > 0)
+            	{	
+            		temp_index++;
+            		s_index++;
+            		continue;
+            	}
+            	else
+            	{
+            		panic("error in deescape, invalid escape character\n");	
+            	}
+            }
+            else
+            {
+            	if((s_index + 2) < strlen(s) && (isdigit(s[s_index+1])) && isdigit(s[s_index+2]))
+            	{
+            		//build value on the spot, rather quick and dirty
+            		
+            		int d3 = s[s_index]-48; //'0' has ascii value 48, so subtract that to get numerical val
+            		int d2 = s[s_index+1]-48;
+            		int d1 = s[s_index+2]-48;
+
+					printf("OCTAL DIGITS %d %d %d\n", d3, d2, d1);
+            		
+            		if(d3<8 && d2 <8 && d1<8){
+            			temp[temp_index] = d3*8*8 + d2*8 + d1;
+            			s_index += 3;
+            			temp_index++;
+            			continue;
+            			}
+            		else
+            		{
+            			panic("possibly invalid octal in de-escape\n");
+            		}	
+            			
+            	}
+            	else
+            	{
+            		panic("ERROR de-escaping character\n");
+            		
+            	}
+            }
+        }
+        
+        temp[temp_index] = s[s_index];
+
+        temp_index++;
+        s_index++;
+    }
+    temp[temp_index] = '\0';
+    
+    return temp;
+}
+
 char *extract_string(char *s)
+{
+	//if second char is ", this means we have a string of the form """something"""
+	//otherwise we have 'something' or "something"
+	//NOTE: only true if regexes for the three variants really work.
+	
+	
+	char *tmp = deescape(s);
+	free(s);
+
+	if(s[1] == '"' || s[1] == '\'')
+	{
+		char *ret =  substring(tmp, 3, strlen(tmp)-3);
+		free(tmp);
+		return ret;
+	}
+	else
+	{
+		char *ret =  substring(tmp, 1, strlen(tmp)-1);
+		printf("RET %s\n",ret);
+		free(tmp);
+		printf("RET2 %s\n",ret);
+		return ret;
+	}
+	
+	
+}
+
+char *extract_string_deprecated(char *s)
 {
 	//if second char is ", this means we have a string of the form """something"""
 	//otherwise we have 'something' or "something"
