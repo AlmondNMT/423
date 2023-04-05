661577ab192245cc0399d711fe353168422e9e5e
Taylor Grover <tgrover@cc.nmt.edu>
2023-04-05 17:28:03 -0600

    - Adding tests/ - Removed tests from .gitignore

---
+++ tests/errors/lex_test1.py
@@ -0,0 +1,6 @@
+# lex_test1.py
+def main():
+    var_! = 5
+    print(var_!)
+
+main()
---
+++ tests/errors/syn_test1.py
@@ -0,0 +1,7 @@
+# syn_test1.py
+def main():
+    x = 5
+    if x > 2
+        print("x is greater than 2")
+
+main()
