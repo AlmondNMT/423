8cc3f3e3a468932a74a63ffe4b6f497232a16cc7
とうふ <jdmmenz@gmail.com>
2023-04-10 17:08:43 -0600

    okay this one for testrunner.sh if not then sudoku

--- testrunner.sh
+++ testrunner.sh
@@ -1,4 +1,13 @@
-#! /bin/bash
+#!/bin/bash
+
+# Before running this script, you may need to convert the line endings
+# to Unix format using the 'dos2unix' command:
+#
+#   dos2unix testrunner.sh
+#
+# Then, you can run the script using the following command:
+#
+#   ./testrunner.sh
 
 for file in 107/*.py
 do
